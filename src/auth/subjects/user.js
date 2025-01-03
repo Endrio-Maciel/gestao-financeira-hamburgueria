65117257226102fdc5fc80847764a5b89ef3f8ee
Endrio Maciel <endriomaciel@gmail.com>
2025-01-03 14:00:44 -0300

    03

--- src/auth/subjects/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.usertSubject = void 0;
-const zod_1 = require("zod");
-exports.usertSubject = zod_1.z.tuple([
-    zod_1.z.literal("get"),
-    zod_1.z.literal("User"),
-]);
